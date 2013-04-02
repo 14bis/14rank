1a696f6557baaa7972ab76726efee71e906c97f9
Ighor Silva <ighoor@gmail.com>
2013-04-02 10:35:53 -0300

    Adds translations and jquery plugin for stars

--- app/models/spree/review.rb
+++ app/models/spree/review.rb
@@ -1,5 +1,5 @@
 class Spree::Review < ActiveRecord::Base
   attr_accessible :rating
   belongs_to :user, :class_name => Spree.user_class.to_s
-  validates_numericality_of :rating
+  validates_numericality_of :rating, :greater_than => 0, :less_than_or_equal_to => 5
 end
