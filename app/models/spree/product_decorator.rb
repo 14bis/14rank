012e8659fd16c6370b4eecb465aee327eb6f9d03
Ighor Silva <ighoor@gmail.com>
2013-04-08 10:30:46 -0300

    Shows rating summary for products

--- app/models/spree/product_decorator.rb
+++ app/models/spree/product_decorator.rb
@@ -1,4 +1,8 @@
 Spree::Product.class_eval do
   has_many :reviews, :class_name => "Spree::ProductReview"
   attr_accessible :avg_rating
+
+  def rating_count value
+    Spree::Review.where(:product_id => self, :rating => value).count
+  end
 end
