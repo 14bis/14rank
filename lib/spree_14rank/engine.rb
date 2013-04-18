b1e2175b09a460edbee15231b4727634204b0e9f
Ighor Silva <ighor@swx.com.br>
2013-04-18 15:34:17 -0300

    Loads decorators

--- lib/spree_14rank/engine.rb
+++ lib/spree_14rank/engine.rb
@@ -15,9 +15,14 @@ module Spree14rank
       # require 'decorators'
       # Decorators.register! File.expand_path('../../../', __FILE__)
 
-      Dir.glob(File.join(File.dirname(__FILE__), '../../app/**/*_decorator*.rb')) do |c|
-        Rails.configuration.cache_classes ? require(c) : load(c)
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/**/*_decorator*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
       end
+
+      Dir.glob(File.join(File.dirname(__FILE__), "../../app/overrides/*.rb")) do |c|
+        Rails.application.config.cache_classes ? require(c) : load(c)
+      end
+
       # Spree::Ability.register_ability(Spree::ReviewsAbility)
     end
 
