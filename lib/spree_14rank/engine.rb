d4d26f9b8f97466a4e53fe2c262e75f5f528fa0a
Ighor Silva <ighor@swx.com.br>
2013-04-23 14:46:15 -0300

    Require assets correctly

--- lib/spree_14rank/engine.rb
+++ lib/spree_14rank/engine.rb
@@ -1,15 +1,10 @@
 module Spree14rank
   class Engine < Rails::Engine
     require 'spree/core'
-    isolate_namespace Spree
+    # isolate_namespace Spree
     engine_name 'spree_14rank'
 
     config.autoload_paths += %W(#{config.root}/lib)
-
-    # use rspec for tests
-    config.generators do |g|
-      g.test_framework :rspec
-    end
 
     def self.activate
       # require 'decorators'
